8556b2dc70969d0b5ffac7795394a4ce61f68435
alex <windev@nate.com>
2024-03-09 22:30:56 +0900

    frame extraction

--- OpenCVex.cpp
+++ OpenCVex.cpp
@@ -3,46 +3,26 @@
 using namespace std;
 using namespace cv;
 
-Mat img;
-int red, green, blue; 
-int drawing = false;
-
-void on_trackbar(int, void*) {}
-
-void drawCircle(int event, int x, int y, int, void* param)
-{
-    if (event == EVENT_LBUTTONDOWN)
-        drawing = true;
-    else if (event == EVENT_MOUSEMOVE)
-    {
-        if (drawing == true)
-            circle(img, Point(x, y), 3, Scalar(0, 0, 255), 10);
-    }
-    else if (event == EVENT_LBUTTONUP)
-        drawing = false;
-
-    imshow("img", img);
-}
-
 int main()
 {
-
-    img = imread("C:/DevCpp/OpenCvex/light.jpg", IMREAD_COLOR);
-    if (img.empty())
+    // VideoCapture cap(0);  // 웹캠인 경우 
+    VideoCapture cap("C:/DevCpp/OpenCVex/owl.mp4"); 
+    if (!cap.isOpened())
     {
-        cout << "영상을 읽을 수 없음" << endl;
+        cout << "동영상을 읽을 수 없음" << endl; 
     }
 
-    namedWindow("img", 1); 
-    imshow("img", img); 
+    namedWindow("frame", 1); 
 
-    imshow("img", img);
-    setMouseCallback("img", drawCircle);
-    createTrackbar("R", "img", &red, 255, on_trackbar);
-    createTrackbar("G", "img", &green, 255, on_trackbar);
-    createTrackbar("B", "img", &blue, 255, on_trackbar);
+    for (;;)
+    {
+        Mat frame; 
+        cap >> frame; 
+        imshow("frame", frame); 
+        if (waitKey(100) >= 0)
+            break;
+    }
 
-    waitKey(0);
     return 0;
 }
 
---
+++ 프레임추출_슬로비디오.cpp
@@ -0,0 +1,30 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    // VideoCapture cap(0);  // 웹캠인 경우 
+    VideoCapture cap("C:/DevCpp/OpenCVex/owl.mp4"); 
+    if (!cap.isOpened())
+    {
+        cout << "동영상을 읽을 수 없음" << endl; 
+    }
+
+    namedWindow("frame", 1); 
+
+    for (;;)
+    {
+        Mat frame; 
+        cap >> frame; 
+        imshow("frame", frame); 
+        if (waitKey(100) >= 0)
+            break;
+    }
+
+    return 0;
+}
+
+
+
